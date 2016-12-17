5e03e36cd89fb9fe1b79870af0fcd81cbcd598dd
jonathb <jonathan.battle@gmail.com>
2016-12-17 10:31:02 +0000

    commit

---
+++ hello.go
@@ -0,0 +1,26 @@
+
+package main
+
+
+
+import (
+
+        "fmt"
+
+
+
+ 	"github.com/jonathb/stringutil"
+
+)
+
+
+
+
+
+func main() {
+
+    fmt.Printf(stringutil.Reverse("hello, world\n") + "\n")
+
+}
+
+
