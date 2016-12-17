7f12978cd065eb5b20ca4e7d3596ddb63777c400
jonathb <jonathan.battle@gmail.com>
2016-12-17 11:55:52 +0000

    3rd commit

--- hello.go
+++ hello.go
@@ -5,6 +5,7 @@ package main
 
 import (
 
+        "os"
         "fmt"
 
 
@@ -19,8 +20,17 @@ import (
 
 func main() {
 
-    fmt.Printf(stringutil.Reverse("hello, world\n") + "\n")
+    wholecomlin := os.Args
+    fmt.Println(wholecomlin)
+    justargs := os.Args[1:]
+    fmt.Println(justargs)
+    numargs := len(os.Args)
+    fmt.Println(numargs)
 
+    arg := os.Args[3]
+    fmt.Println(arg)
+
+    fmt.Printf(stringutil.Reverse("hello, world\n") + "\n")
 }
 
 
